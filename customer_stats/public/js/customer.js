e4f6aa69657dad3f89a10da344deb22f9282a279
rohankumbhar-01 <rkrohankumbhar@gmail.com>
2024-11-18 18:09:45 +0530

    del: removed event button code from it

--- customer_stats/public/js/customer.js
+++ customer_stats/public/js/customer.js
@@ -1,105 +1,6 @@
 frappe.ui.form.on("Customer", {
     refresh: function (frm) {
-        frm.events.make_custom_buttons(frm);
-    },
-    make_custom_buttons: function (frm) {
-        frm.add_custom_button("Event", () => {
-            let d = new frappe.ui.Dialog({
-                title: 'Enter details',
-                fields: [
-                    {
-                        label: 'Category',
-                        fieldname: 'event_category',
-                        fieldtype: 'Select',
-                        options:["Event","Meeting","Call","Sent/Received Email","Other"],
-                        default:"Event"
-                    },
-                    {
-                        label: 'Next Step',
-                        fieldname: 'next_step',
-                        fieldtype: 'Select',
-                        options:[
-                            "Qualify Customer",
-                            "Qualify Decision Maker",
-                            "Ask for Filter List / Frequency / Price",
-                            "Proposal Follow-Up / Ask for Order",
-                            "Schedule Reorder / Follow-Up / Blanket Order",
-                            "Follow-Up"],
-                        reqd:1
-                    },
-                    {
-                        label: 'Public',
-                        fieldname: 'public',
-                        fieldtype: 'Check',
-                        default:1
-                    },
-                    {
-                        fieldname: 'column_break_123',
-                        fieldtype: 'Column Break'
-                    },
-                    {
-                        label: 'Date',
-                        fieldname: 'date',
-                        fieldtype: 'Datetime',
-                        reqd:1
-                    },
-                    {
-                        label: 'Assigned To',
-                        fieldname: 'assigned_to',
-                        fieldtype: 'Link',
-                        options:'User'
-                    },
-                    {
-                        fieldname: 'section_break_123',
-                        fieldtype: 'Section Break'
-                    },
-                    {
-                        label: 'Summary',
-                        fieldname: 'summary',
-                        fieldtype: 'Data',
-                        reqd:1
-                    },
-                    {
-                        fieldname: 'section_break_1234',
-                        fieldtype: 'Section Break'
-                    },
-                    {
-                        label: 'Description',
-                        fieldname: 'description',
-                        fieldtype: 'Text Editor'
-                    },
-                    {
-                        fieldname: 'section_break_1234',
-                        fieldtype: 'Section Break'
-                    },
-                    {
-                        label: 'Attachment',
-                        fieldname: 'attachment',
-                        fieldtype: 'Attach'
-                    },
 
-                ],
-                size: 'large', // small, large, extra-large 
-                primary_action_label: 'Submit',
-                primary_action(values) {
-                    frappe.call({
-                        method:"florence.quotation.update_doc",
-                        args:{
-                            "values":values,
-                            "reference_docname":frm.doc.name,
-                            "reference_doctype":frm.doc.doctype
-                        },
-                        callback:function(r){
-                            frm.refresh()
-                            
-                        }
-                    })
-                    d.hide();
-                }
-            });
-            
-            d.show();
-        })
     }
 })
 
@@ -151,8 +52,8 @@ $.extend(erpnext.utils, {
                                         r.message.previous_year_billing,
                                         company_wise_info[0].currency
                                     )
-                                ]), 'blue');
-                                
+                                ]), 
+                            'blue');      
                         }
                     }
                 })
